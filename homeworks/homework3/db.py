4fadc54c264b6352eb699088c862c3a5798f673b
qjari calizaya <qjarishekka@outlook.com>
2025-10-18 01:00:48 +0300

    homework3

---
+++ homeworks/homework3/db.py
@@ -0,0 +1,153 @@
+﻿import os
+import sqlite3
+from dotenv import load_dotenv
+from datetime import datetime, date
+load_dotenv()
+
+DB_PATH = os.getenv("DB_PATH", "bot.db")
+
+def _connect():
+    conn = sqlite3.connect(DB_PATH, timeout=5.0)
+    conn.row_factory = sqlite3.Row
+    conn.execute("PRAGMA foreign_keys = ON")
+    conn.execute("PRAGMA journal_mode = WAL")
+    conn.execute("PRAGMA busy_timeout = 5000")
+    return conn
+
+def init_db():
+    schema = """
+    CREATE TABLE IF NOT EXISTS notes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    text TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+    """
+    with _connect() as conn:
+        conn.executescript(schema)
+
+
+def add_note(user_id: int, text: str) -> int:
+    with _connect() as conn:
+        cur = conn.execute(
+            "INSERT INTO notes(user_id, text) VALUES (?, ?)",
+            (user_id, text)
+        )
+    return cur.lastrowid
+
+
+def list_notes(user_id: int, limit: int = 10):
+    with _connect() as conn:
+        cur = conn.execute(
+            """SELECT id, text, created_at
+            FROM notes
+            WHERE user_id = ?
+            ORDER BY id DESC
+            LIMIT ?""",
+            (user_id, limit)
+        )
+    return cur.fetchall()
+
+def find_notes(user_id : int , text:str) -> bool:
+    with _connect() as conn:
+        cur = conn.execute(
+            """SELECT id, text, created_at
+            FROM notes
+            WHERE user_id = ?
+            AND text LIKE '%'|| ? || '%' 
+            ORDER BY id DESC
+            LIMIT 10;""", 
+            (user_id, text)
+
+        )
+        return cur.fetchall()
+    
+
+
+def update_note(user_id: int, note_id: int, text: str) -> bool:
+    with _connect() as conn:
+        cur = conn.execute(
+            """UPDATE notes
+            SET text = ?
+            WHERE user_id = ? AND id = ?""",
+            (text, user_id, note_id)
+        )
+    return cur.rowcount > 0
+
+
+def delete_note(user_id: int, note_id: int) -> bool:
+    with _connect() as conn:
+        cur = conn.execute(
+            "DELETE FROM notes WHERE user_id = ? AND id = ?",
+            (user_id, note_id)
+        )
+    return cur.rowcount > 0
+
+def count_note(user_id: int):
+    with _connect() as conn:
+        cur = conn.execute(
+            "SELECT COUNT(*) FROM notes WHERE user_id = ?",
+            (user_id,)
+        )
+    
+    row = cur.fetchone()
+    return row[0] if row else 0
+
+# возвращает текст со всей активностью за последние 7 дней
+def activity_note(user_id:int):
+    with _connect() as conn:
+        cur = conn.execute(
+            "SELECT created_at FROM notes WHERE user_id = ?",
+            (user_id, )
+        )
+        dataTime = [datetime.fromisoformat(row["created_at"]) for row in cur.fetchall()]
+    
+    # список активности по дням недели (0=понедельник, 6=воскресенье)
+    activityInDays = [0,0,0,0,0,0,0]
+    for day in dataTime:
+        activityInDays[day.weekday()] += 1
+    return activityToASCII(activityInDays)
+    
+# форматирует числа с помощью ASCII для отображения активности
+def activityToASCII(activityInDays):
+    text = "активность:"
+    left = "-"
+    right = "-|"
+    days = ["M","T","W","T","F","S","S"]
+    today = date.today().weekday() + 1
+    step = today
+    stop = today
+    text += "\n|"
+    
+    # создание верхней линии с днями недели
+    while True:
+        if step > 6 : step = 0
+        text = text + left + days[step] + right
+        step+=1
+        if step == stop : 
+            text+="\n|"
+            break
+    
+    # добавление количества активности под соответствующими днями
+    step = today
+    while True:
+        if step > 6 : step = 0
+        text+=  parseIntToString(activityInDays[step]) + "|"
+        step+=1
+        if step == stop : 
+            break
+
+    return text
+    
+# определяет формат отображения числа --1, -10, 100
+def parseIntToString(number):
+
+    digits = len(str(number))
+    #print(digits)
+    
+    if(digits==3):
+        return number
+    if(digits==2):
+        return f"-{number}"
+    if(digits==1):
+        return f"--{number}"
---
+++ homeworks/homework3/main.py
@@ -0,0 +1,177 @@
+﻿import os
+import telebot
+from telebot import types
+from dotenv import load_dotenv
+import logging
+from db import *
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN") or ""
+
+init_db()
+
+def parse_ints_from_text(text:str) -> list[int]:
+    text = text.replace(",", " ")
+    tokens = [t for t in text.split() if not t.startswith("/")]
+    return [int(t) for t in tokens if t.strip().lstrip("-").isdigit()]
+
+if not TOKEN:
+    raise RuntimeError("there isn't TOKEN in .env")
+
+bot = telebot.TeleBot(TOKEN)
+
+
+def setup_bot_commands():
+    commands = [
+        types.BotCommand("start","Запуск"),
+        types.BotCommand("note_add","add"),
+        types.BotCommand("note_list","show"),
+        types.BotCommand("note_find","find"),
+        types.BotCommand("note_edit","edit"),
+        types.BotCommand("note_de","delete"), 
+        types.BotCommand("note_count","count"), 
+        types.BotCommand("activity","activity"),
+
+    ]
+
+    bot.set_my_commands(commands)
+
+
+""" def make_main_kb()->types.ReplyKeyboardMarkup:
+    kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    kb.row("О боте" , "Сумма")
+    kb.row("/help")
+    return kb """
+
+# функция запуска
+@bot.message_handler(commands=['start' , 'help'])
+def start_help(message):
+    welcome_text = "Привет!!, я учебный бот который сохраняет список"
+    bot.reply_to(message,welcome_text)
+
+
+# функция для добавления заметки
+@bot.message_handler(commands=['note_add'])
+def note_add(message):
+
+    if(count_note(message.from_user.id) > 6 ):
+        bot.send_message(message.chat.id, "ты уже достиг лимит 50 записей")
+    else:
+        bot.send_message(message.chat.id, "Введи текст заметки")
+        bot.register_next_step_handler(message, save_note)
+
+# функция для сохранения заметки и обратной связи
+def save_note(message):
+    user_id = message.from_user.id
+    text = message.text.strip()
+
+    if not text:
+        bot.reply_to(message, "Текст пустой, попробуй снова.")
+        return
+
+    note_id = add_note(user_id, text) 
+    bot.reply_to(message, f"Заметка сохранена (id={note_id})" )
+
+
+# функция для возвращения всех заметок
+@bot.message_handler(commands=['note_list'])
+def note_list(message):
+    note = list_notes(message.from_user.id)
+    text = ""
+    for note_id, note_text, created_at in note:
+        text += f"\n[{note_id}] {note_text} ({created_at})"
+
+    bot.send_message(message.chat.id, text )
+
+
+# функция для поиска заметки по слову
+@bot.message_handler(commands=['note_find'])
+def note_find(message):
+    bot.send_message(message.chat.id, "введи текст для поиска")
+    bot.register_next_step_handler(message,find )
+
+def find(message):
+    note = find_notes(message.from_user.id , message.text)
+    text = ""
+    for note_id, note_text, created_at in note:
+        text += f"\n[{note_id}] {note_text} ({created_at})"
+
+    bot.send_message(message.chat.id, text)
+
+
+# функция для замены заметки по ID и новому тексту
+@bot.message_handler(commands=['note_edit'])
+def note_edit(message):
+    bot.send_message(message.chat.id, "Введи ID заметки, которую хочешь изменить")
+    bot.register_next_step_handler(message, edit_step)
+
+# обратная связь при редактировании
+def edit_step(message):
+    user_id = message.from_user.id
+    try:
+        note_id = int(message.text.strip())
+    except ValueError:
+        bot.reply_to(message, "ID должен быть числом")
+        return
+    
+    bot.send_message(message.chat.id, "Введи новый текст")
+    bot.register_next_step_handler(message, update_step, note_id)
+
+
+def update_step(message, note_id):
+    user_id = message.from_user.id
+    new_text = message.text.strip()
+
+    if not new_text:
+        bot.reply_to(message, "Текст пустой")
+        return
+
+    if update_note(user_id, note_id, new_text):
+        bot.reply_to(message, f"Заметка {note_id} обновлена")
+    else:
+        bot.reply_to(message, "Не удалось обновить заметку (проверь ID)")
+
+
+
+# функция для удаления заметки
+@bot.message_handler(commands=['note_de'])
+def note_delete(message):
+    bot.send_message(message.chat.id, "введите ID")
+    bot.register_next_step_handler(message,delete )
+
+def delete(message):
+    userID = message.from_user.id
+    try:
+        note_id = int(message.text.strip())
+    except ValueError:
+        bot.send_message(message.chat.id, "ID должен быть числом")
+        return
+    if delete_note(userID, note_id):
+        bot.send_message(message.chat.id, "Заметка удалена")
+    else:
+        bot.send_message(message.chat.id, "Не удалось удалить заметку")
+
+# функция чтобы узнать сколько у тебя заметок
+@bot.message_handler(commands=['note_count'])
+def note_count(message):
+    count = count_note(message.from_user.id)
+    text = plural_ru(count, ('запись', 'записи', 'записей'))
+    bot.send_message(message.chat.id, f"Ты записал {count} {text}")
+    
+def plural_ru(count, forms):
+    if count % 10 == 1 and count % 100 != 11:
+        return forms[0]
+    elif 2 <= count % 10 <= 4 and not (12 <= count % 100 <= 14):
+        return forms[1]
+    else:
+        return forms[2]
+
+# функция для просмотра вашей активности с этим ботом
+@bot.message_handler(commands=['activity'])
+def check_activity(message):
+    bot.send_message(message.chat.id,activity_note(message.from_user.id) )
+
+
+if __name__ == "__main__":
+ setup_bot_commands()
+ bot.infinity_polling(skip_pending=True)
